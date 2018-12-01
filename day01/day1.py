10e683f4cc959ea271ac1041c8efee9d46f1cfbb
April Jackson <april.jackson@student.ufv.ca>
2018-11-30 21:36:31 -0800

    Solved day 1 part 2

--- day01/day1.py
+++ day01/day1.py
@@ -16,5 +16,25 @@ def part1(freq):
     print('Part 1: Final Frequency: {}'.format(freq))
 
 
+# Part 2
+def part2(freqs):
+    freqs = [int(x) for x in freqs.splitlines()]
+    freq_dup = find_dup_freq(freqs, 0)
+    print('Part 2: Frequency Reached Twice: {}'.format(freq_dup))
+
+
+# Part 2 Helper
+def find_dup_freq(freqs, freq_cur):
+    freq_res = {}
+    while True:
+        for i in freqs:
+            freq_cur += i
+            if freq_cur in freq_res:
+                return freq_cur
+            else:
+                freq_res[freq_cur] = True
+
+
 # Do the stuff
 part1(data)
+part2(data)
