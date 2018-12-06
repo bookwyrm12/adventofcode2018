fe248ed9a46aa5196fb9ef4c1c66ef82873ae7d2
April Nickel <april.jackson012@gmail.com>
2018-12-05 20:14:26 -0800

    Solve day 5 part 1

---
+++ day05/day5.py
@@ -0,0 +1,31 @@
+#################################
+#  Advent of Code 2018          #
+#  Day 5: Alchemical Reduction  #
+#  April Nickel                 #
+#################################
+
+
+# Get the data
+with open('day5.in') as f:
+    data = f.read()
+
+
+# Part 1
+def part1(polymer):
+    polymer = polymer.strip()
+    polymer_new = []
+    poly_ptr = 0
+    for i in range(len(polymer)):
+        polymer_new.append(polymer[i])
+        if len(polymer_new) > 1:
+            # 32 is the distance between the upper- & lower-case ord of the same char
+            if abs(ord(polymer_new[-1]) - ord(polymer_new[-2])) == 32:
+                polymer_new.pop()
+                polymer_new.pop()
+    
+    answer = len(polymer_new)
+    print('Part 1: # Remaining Units: {}'.format(answer))
+
+
+# Do the stuff
+part1(data)
